aa6f26c889df8f95b0582f392108e66ffe8fdb0f
jakiadp <jakiadp@gmail.com>
2025-10-01 23:33:55 +0600

    add to file

---
+++ src/Component/liy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const liy = () => {
+    return (
+        <div>
+                   <h3>koi tui vai</h3>            
+        </div>
+    );
+};
+
+export default liy;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>koi vai tumi</h1>
+            <h3>koi vi tui</h3>
         </div>
     );
 };
